8ca35b780118fa3bec109da56aa35a6456aeb6f3
Henry Dieckhaus (dieckhau) <dieckhau@longleaf-login4.its.unc.edu>
2026-02-11 14:02:26 -0500

    Removed REDUCE submodule. Added minimal install script.

---
+++ install_hbdesigner.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+
+source ~/.bashrc
+# Make HBDesigner environment
+mamba env create -n hbdesigner python=3.10
+
+# Install HBDesigner and dependencies (note this order matters)
+mamba activate hbdesigner
+
+# 1. torch 2.8 for CUDA 12.8
+pip install torch==2.8
+
+# 2. torch-geometric and scatter (need to install torch first)
+pip install torch_geometric
+pip install torch_scatter -f https://data.pyg.org/whl/torch-2.8.0+cu128.html
+
+# 3. Other dependencies
+pip install omegaconf biopython wandb ruff pytest "numpy==1.26.4" scipy networkx pandas pebble
+
+# 4. PyRosetta (install via installer script)
+pip install pyrosetta-installer; python -c 'import pyrosetta_installer; pyrosetta_installer.install_pyrosetta(distributed=True)'
+
+# 5. HBDesigner pip install
+pip install -e .
+
+# 6. Optional dependencies only used for development/benchmarking:
+# pip install ruff pytest hydride biotite
